cdce0cf5e64fad8d36575c43d6ddb3837019fcda
wassimouricha <wbouricha5@gmail.com>
2022-04-13 14:06:06 +0200

    rawer fixed but still ain't authin the drawer

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -6,8 +6,7 @@ import 'package:newsappwassim/const.dart';
 import 'package:newsappwassim/model.dart';
 import 'package:newsappwassim/news_api.dart';
 import 'package:bubble_bottom_bar/bubble_bottom_bar.dart';
-import 'package:newsappwassim/user_page.dart';
-import 'package:newsappwassim/home_screen.dart';
+import 'package:newsappwassim/drawer.dart';
 import 'package:newsappwassim/delayed_animation.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -71,7 +70,7 @@ class _HomeScreenState extends State<Login> {
             )),
         titleSpacing: 0,
       ),
-      drawer: const NavigationDrawer(),
+      drawer: const NavigationDraweer(),
       body: const connexionPage(),
     );
   }
@@ -250,111 +249,3 @@ class _connexionPageState extends State<connexionPage> {
 }
 
 
-
-//mon drawer = ma sidebar
-
-class NavigationDrawer extends StatelessWidget {
-  const NavigationDrawer({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) => Drawer(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            buildHeader(context),
-            buildMenuItems(context),
-          ],
-        ),
-      );
-}
-
-//le widget du header du drawer/sidebar
-Widget buildHeader(BuildContext context) => Material(
-      color: Colors.black,
-      child: InkWell(
-        onTap: () {
-          //pour fermer le navigation drawer
-          Navigator.pop(context);
-          //pour rediriger vers la page
-          Navigator.of(context).push(MaterialPageRoute(
-            builder: (context) => const UserPage(),
-          ));
-        },
-        child: Container(
-          padding: EdgeInsets.only(
-            top: 24 + MediaQuery.of(context).padding.top,
-            bottom: 24,
-          ),
-          child: Column(
-            children: const [
-              CircleAvatar(
-                radius: 52,
-                backgroundImage: NetworkImage(
-                    'https://media-exp1.licdn.com/dms/image/D4E03AQHKyal9OiD12g/profile-displayphoto-shrink_800_800/0/1648624925960?e=2147483647&v=beta&t=NimRdFpaBcn7mrK3Abem2USfCRhEsZ8K7-h8NAQ9xYY'),
-              ),
-              SizedBox(
-                height: 12,
-              ),
-              Text(
-                "Wassim Bouricha",
-                style: TextStyle(fontSize: 25, color: Colors.white),
-              ),
-              SizedBox(
-                height: 12,
-              ),
-              Text(
-                "wbouricha5@gmail.com",
-                style: TextStyle(fontSize: 15, color: Colors.white),
-              ),
-              SizedBox(
-                height: 12,
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-
-//le widget du menu du drawer/sidebar
-Widget buildMenuItems(BuildContext context) => Wrap(
-  runSpacing: 16, //espace vertical
-  children: [
-    ListTile(
-      leading: const Icon(Icons.home_outlined),
-      title: const Text(
-          'Accueil'), //je peux remplacement pushreplacement par push pour avoir le bouton en haut a gauche pour revenir
-      //il faut cependant ajouter Navigator.pop(context); pour que le drawer se ferme lorsque l'on va revenir sur la page
-      onTap: () => Navigator.of(context).pushReplacement(
-          MaterialPageRoute(builder: (context) => const HomeScreen())),
-    ),
-    ListTile(
-      leading: const Icon(Icons.favorite_border),
-      title: const Text('Notifications'),
-      onTap: () {},
-    ),
-    ListTile(
-      leading: const Icon(Icons.login),
-      title: const Text('Connexion'),
-      onTap: () => Navigator.of(context).pushReplacement(
-          MaterialPageRoute(builder: (context) => const Login())),
-    ),
-    InkWell(
-      borderRadius: BorderRadius.circular(500),
-      splashColor: Colors.black,
-      onTap: () {
-        //pour fermer le drawer
-        Navigator.of(context).pop();
-      },
-      child: const Center(
-        child: CircleAvatar(
-          radius: 20,
-          backgroundColor: Colors.black,
-          child: Icon(Icons.arrow_back, color: Colors.white),
-        ),
-      ),
-    ),
-    const Divider(
-      color: Colors.black,
-    ),
-  ],
-);

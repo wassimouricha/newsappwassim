bcc0ed9aa80ec248a4c9c4d25d70461b61ddb208
wassimouricha <wbouricha5@gmail.com>
2022-04-14 15:40:04 +0200

    adding a forget password page

--- lib/password.dart
+++ lib/password.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: avoid_print, camel_case_types
 
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:newsappwassim/const.dart';
 import 'package:newsappwassim/model.dart';
@@ -10,17 +9,16 @@ import 'package:bubble_bottom_bar/bubble_bottom_bar.dart';
 import 'package:newsappwassim/drawer.dart';
 import 'package:newsappwassim/delayed_animation.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:newsappwassim/signuppage.dart';
-import 'package:newsappwassim/password.dart';
 
-class Login extends StatefulWidget {
-  const Login({Key? key}) : super(key: key);
+
+class ForgotPassword extends StatefulWidget {
+  const ForgotPassword({Key? key}) : super(key: key);
 
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<Login> {
+class _HomeScreenState extends State<ForgotPassword> {
   List<NewsApiModel>? newsList;
   bool isLoading = true;
   var currentIndex = 0;
@@ -92,9 +90,6 @@ class _connexionPageState extends State<connexionPage> {
 
   final passwordController = TextEditingController();
 
-  var _obscureText = true;
-  //obscuretext est une propriété qui lorsque elle passe a true obscurcit le champ de texte
-//la fonction future sign in ici indique que lorsque qu'on activeras la fonction
   Future signIn() async {
     await FirebaseAuth.instance.signInWithEmailAndPassword(
       email: emailController.text.trim(),
@@ -117,13 +112,14 @@ class _connexionPageState extends State<connexionPage> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
+                    const SizedBox(height: 120),
                     delayedAnimation(
                       delay: 1000,
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Text(
-                            "Connectez vous avec votre adresse mail",
+                            "Recevez un Email pour reset votre mot de passe ",
                             textAlign: TextAlign.center,
                             style: GoogleFonts.poppins(
                               color: Colors.black,
@@ -150,61 +146,24 @@ class _connexionPageState extends State<connexionPage> {
                                     ),
                                   ),
                                 ),
-                                const SizedBox(height: 30),
-                                delayedAnimation(
-                                  delay: 2000,
-                                  child: TextFormField(
-                                    controller: passwordController,
-                                    obscureText: _obscureText,
-                                    decoration: InputDecoration(
-                                      labelStyle: TextStyle(
-                                        color: Colors.grey[400],
-                                      ),
-                                      labelText: 'Mot de passe',
-                                      suffixIcon: IconButton(
-                                        icon: const Icon(
-                                          Icons.visibility,
-                                          color: Colors.black,
-                                        ),
-                                        onPressed: () {
-                                          setState(() {
-                                            _obscureText = !_obscureText;
-                                          });
-                                        },
-                                      ),
-                                    ),
-                                      autovalidateMode: AutovalidateMode
-                                          .onUserInteraction, //fonction permettant de montrer le text
-                                      validator: (value) => value != null &&
-                                              value.isEmpty
-                                          ? "Entrer votre mot de passe est obligatoire. "
-                                          : null, // ici si la taille du mot de passe n'est pas supérieur ou égal a 6  alors le message  s'afficheras ou alors le mdp est valide
-                                  ),
-                                ),
+                             
+                              
                               ],
                             ),
                           ),
-                          const SizedBox(height: 125),
+                          const SizedBox(height: 25),
                           delayedAnimation(
-                            delay: 3000,
+                            delay: 2000,
                             child: Container(
                               margin: const EdgeInsets.symmetric(
                                   horizontal: 30, vertical: 10),
                               height: 200,
                               child: Column(
                                 children: [
-                                  Text(
-                                    "Il est recommandé de connecter votre adresse mail afin que nous puissions protéger vos données personnelles.",
-                                    textAlign: TextAlign.center,
-                                    style: GoogleFonts.poppins(
-                                      color: Colors.grey,
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.w400,
-                                    ),
-                                  ),
-                                  const SizedBox(height: 30),
+                            
+                                  const SizedBox(height: 35),
                                   delayedAnimation(
-                                    delay: 3500,
+                                    delay: 2500,
                                     child: Container(
                                       margin: const EdgeInsets.symmetric(
                                           horizontal: 40, vertical: 14),
@@ -235,55 +194,8 @@ class _connexionPageState extends State<connexionPage> {
                                       ]),
                                     ),
                                   ),
-                                  delayedAnimation(
-                                    delay: 3500,
-                                    child: Container(
-                                      margin: const EdgeInsets.symmetric(
-                                          horizontal: 40, ),
-                                      child: Column(children: [
-                                        GestureDetector(
-                                          child: const Text("Vous avez oublié votre mot de passe ?", 
-                                          style: TextStyle(
-                                            decoration: TextDecoration.underline,
-                                            color: Colors.red,
-
-                                          ),
-                                          ),
-                                          onTap: () =>Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => const ForgotPassword())) ,
-                                         ),
-                                      ]),
-                                    ),
-                                  ),
+                                 
                                 
-                                  delayedAnimation(
-                                    delay: 3700,
-                                    child: Container(
-                                      margin: const EdgeInsets.symmetric(
-                                          horizontal: 40, vertical: 2),
-                                      child: Column(
-                                        children: [
-                                          RichText(
-                                            text:  TextSpan(
-                                                style: const TextStyle(
-                                                    color: Colors.black),
-                                                text: "Pas de compte ? ",
-                                                children: [
-                                                  TextSpan(
-                                                    recognizer:  TapGestureRecognizer()..onTap = () => Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => const Signup())) ,
-                                                      
-                                                    text: "Inscrivez-vous",
-                                                    style: const TextStyle(
-                                                      decoration: TextDecoration
-                                                          .underline,
-                                                      color: Colors.red,
-                                                    ),
-                                                  ),
-                                                ]),
-                                          )
-                                        ],
-                                      ),
-                                    ),
-                                  ),
                                 ],
                               ),
                             ),
@@ -299,3 +211,4 @@ class _connexionPageState extends State<connexionPage> {
         ));
   }
 }
+// TODO Implement this library.
